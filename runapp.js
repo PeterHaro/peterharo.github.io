f1030e546788e8ce769afd262e4d4a224af66326
Peter Haro <peter.haro@sintef.no>
2017-06-01 13:48:42 +0200

    soon reeverted

--- runapp.js
+++ runapp.js
@@ -1,4 +1,4 @@
-d3.json(jsonLD, (err, data) => {
+d3.json('jsonld.json', (err, data) => {
   if (err) return console.warn(err);
   d3.jsonldVis(data, '#graph', { w: 800, h: 600, maxLabelWidth: 250 });
 });
